8a3a5def1d53a6e935412beadb1459c4482b6391
chrisgaines47 <chrisgaines47@gmail.com>
2024-06-26 23:38:07 -0700

    adding two way binding

---
+++ testing/test.js
@@ -0,0 +1,83 @@
+var logTypes = {
+    reset: '\x1b[0m',
+    green: '\x1b[32m',
+    red: '\x1b[31m',
+    cyan: '\x1b[36m',
+    underscore: '\x1b[4m'
+}
+
+function logSuccess(text) {
+    console.log(`${logTypes.green}%s${logTypes.reset}`, text);
+}
+function logError(text) {
+    console.log(`${logTypes.red}%s${logTypes.reset}`, text);
+}
+function logSuite(text) {
+    console.log(`${logTypes.cyan}${logTypes.underscore}%s${logTypes.reset}`, text);
+}
+
+
+class Matchers {
+    constructor(actual) {
+        this.actual = actual;
+
+    }
+
+    toBe(expected) {
+        if (expected === this.actual) {
+        } else {
+            throw new Error(`Actual: ${this.actual}, Expected: ${expected}`);
+        }
+    }
+
+    toBeTruthy() {
+        if (actual) {
+        } else {
+            throw new Error(`Expected value to be truthy but got ${this.actual}`);
+        }
+    }
+}
+  
+  function expect(actual) {
+    return new Matchers(actual);
+  }
+  
+  function describe(suiteName, fn) {
+    try {
+      logSuite(`suite: ${suiteName}`);
+      fn();
+    } catch(err) {
+      logError(err.message);
+    }
+  }
+  
+  function it(testName, fn) {
+    process.stdout.write(`test: ${testName}  `);
+    try {
+      fn();
+        logSuccess(`✓`);
+    } catch (err) {
+        logError(`⤫`);
+      logError(err.message);
+    }
+  }
+  
+  describe('a suite', () => {
+    it('a test that will fail', () => {
+      expect(true).toBe(false);
+    })
+  
+    it('a test that will never run', () => {
+      expect(1).toBe(1);
+    })
+  })
+  
+  describe('another suite', () => {
+    it('should succeed, true === true', () => {
+      expect(true).toBe(true);
+    })
+  
+    it('should succeed, 1 === 1', () => {
+      expect(1).toBe(1);
+    })
+  })
