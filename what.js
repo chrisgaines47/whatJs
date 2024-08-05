44e58fa536f7f76813d26725f0822b8008371de8
chrisgaines47 <chrisgaines47@gmail.com>
2024-08-05 12:47:35 -0700

    Context data change

--- what.js
+++ what.js
@@ -4,6 +4,10 @@ const whatJs = {
     addChild: (el, child) => whatJs.isInsert(child) ? el.insertAdjacentHTML("beforeend", child) : el.appendChild(child),
     isInput: node => ["INPUT", "TEXTAREA", "SELECT"].includes(node.nodeName),
     checkInsertable: arg => (arg instanceof Array) || (arg instanceof Element) || whatJs.isInsert(arg),
+    objProxy: (obj, _context) => new Proxy(obj, {
+        get: (target, key) => whatJs.isObj(target[key]) ? objProxy(target[key]) : target[key],
+        set: (target, key, value) => { target[key] = value; _context.subscribers.forEach(fn => fn(target, key)); return true;}
+    }),
 
     dom: new Proxy({registry: new Map(), tags: new Map()}, {
         get: (o, key) => (props = {}, children = [], events = {}) => {
@@ -28,7 +32,7 @@ const whatJs = {
                     else props?.updateSuccess?.(el, props.modelkey, props.model);
                 });
 
-            if(props.rerender && props.model) props.model.subscribe(`${props.model.props.name}-${props?.id || 'noId'}-rerender`,
+            if(props.rerender && props.model) props.model.subscribe(
                 updateData => document.body.contains(el.parentNode) && el.parentNode.replaceChildren(props.rerender(updateData)));
 
             props?.wait?.().then(props?.waitSuccess, props?.waitError).then(result => result && el.replaceChildren(result));
@@ -41,9 +45,11 @@ const whatJs = {
     }),
     context: new Proxy({_contexts: {}}, {
         get: function(whatProxy, contextName) {
-            whatProxy._contexts[contextName] ??= new Proxy({name: contextName, actions: new Map(), data: {},
+            whatProxy._contexts[contextName] ??= new Proxy({name: contextName, actions: new Map(), subscribers: new Map(),
+                data: whatJs.objProxy({}),
                 expose: (fnKey, fn, data={}) => context.actions.set(fnKey,{fn, data}) || (() => {context.actions.delete(fnKey)}),
-                utilize: actionKey => context.actions.get(actionKey)
+                utilize: actionKey => context.actions.get(actionKey),
+                subscribe: (id, fn) => context.subscribers.set(id, fn)
             },{
                 get: (invokerContext, prop) => {
                     if(prop in invokerContext) return invokerContext[prop];
@@ -51,18 +57,23 @@ const whatJs = {
                     return fn ? new Proxy(fn, {
                         apply: (l, m, args) =>
                             (typeof args[0] === 'function') ? args[0].apply({},[fn, data]) : fn.apply({}, [...args, data])}) : Reflect.get(...arguments);
+                },
+                set: (target, key, value) => {
+                    if(key === 'data') { let result = target[key] = whatJs.objProxy(value, context); return result; }
+                    return target[key] = value;
                 }
             });
             let context = whatProxy._contexts[contextName];
             return context;
         }
     }),
-    model: new Proxy({
-        _models: {},
+    model: new Proxy({_models: {},
         _makeModel: function(inputData, parent=false) {
-            let schema = {}; let validation = {}; let subscribers = [];
+            let schema = {}; let validation = {}; let subscribers = []; let data;
+            if(!whatJs.isObj(inputData)) return inputData;
+            else data = structuredClone(inputData);
         
-            let proxyObj = whatJs.isObj(inputData) ? new Proxy(inputData, {
+            let proxyObj = new Proxy(data, {
                 get: (target, key) => {
                     if(key === 'validation') return validation;
                     if(key === '_validate') return () => {
@@ -90,9 +101,9 @@ const whatJs = {
                     subscribers.forEach(subscriber => subscriber(target, key));
                     return (parent || proxyObj)._validate();
                 }
-            }) : inputData;
-        
-            whatJs.isObj(inputData) && Object.keys(inputData).forEach(key => proxyObj[key] = whatJs.model._makeModel(inputData[key], parent || proxyObj));
+            });
+            
+            Object.keys(data).forEach(key => proxyObj[key] = whatJs.model._makeModel(data[key], parent || proxyObj));
             return proxyObj;
         }
     }, {
@@ -101,5 +112,7 @@ const whatJs = {
     })
 };
 
-let {model} = whatJs;
+let {model, context} = whatJs;
 model.x = {ok: 'go'}
+context.a.data = {ok: 'go'};
+context.a.subscribe('ok', () => console.log(1))
